dd6ea4b9f7c93b612546cd3ff61cb0acb1898eba
Chris Gramer <gramer3514@gmail.com>
2023-07-19 20:56:00 -0700

    moved the public and src into client folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 
 function App() {
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./index.html",
     // tells tailwind
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./client/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
