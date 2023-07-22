8b8422dd399530bfb84fea364365e9bef78c6e24
Chris Gramer <gramer3514@gmail.com>
2023-07-22 09:39:56 -0700

    placeholder in footer and working on home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,9 @@ const Home = () => {
 
     return (
         <div>
-            Testing
+            <div class="relative h-32 w-32 ...">
+                <div class="absolute inset-y-0 left-0 w-16 ...">04</div>
+            </div>
         </div>
     )
 
